5fab19c51ac7e1a91cea12417f7532928b07402b
Benjamin Eder <barny.eder@gmail.com>
2018-06-05 20:03:29 +0200

    Buildable server + Travis integration

--- cie-server/src/main/java/edu/hm/cs/cieserver/campus/CampusRepository.java
+++ cie-server/src/main/java/edu/hm/cs/cieserver/campus/CampusRepository.java
@@ -1,6 +1,8 @@
 package edu.hm.cs.cieserver.campus;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CampusRepository extends JpaRepository<Campus, Long> {
 }
--- cie-server/src/main/java/edu/hm/cs/cieserver/user/UserRepository.java
+++ cie-server/src/main/java/edu/hm/cs/cieserver/user/UserRepository.java
@@ -1,10 +1,12 @@
 package edu.hm.cs.cieserver.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * Repository for the user entity.
  */
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 	User findByEmail(String email);
