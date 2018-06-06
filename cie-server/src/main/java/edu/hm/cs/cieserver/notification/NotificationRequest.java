a2bd4899afbb9a9918501f0b91e970a78aac3a2b
Gleydar <gleydar@outlook.de>
2018-06-06 20:09:34 +0200

    Fixed targetting

--- cie-server/src/main/java/edu/hm/cs/cieserver/notification/NotificationRequest.java
+++ cie-server/src/main/java/edu/hm/cs/cieserver/notification/NotificationRequest.java
@@ -2,18 +2,23 @@ package edu.hm.cs.cieserver.notification;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 public class NotificationRequest {
 
-    //private String to;
+    private String to;
     private String priority = "normal";
     private Map<String, String> notification;
-    private Map<String, String> data = null;
 
-    public NotificationRequest(String title, String content) {
+    private Map<String, String> data;
+
+    public NotificationRequest(String title, String content, Optional<String> to) {
         notification = new HashMap<>();
+        data = new HashMap<>();
         notification.put("title", title);
         notification.put("body", content);
+        data.put("click-action", "FLUTTER_NOTIFICATION_CLICK");
+        this.to = to.orElse("/topics/all");
     }
 
     public String getPriority() {
@@ -38,5 +43,13 @@ public class NotificationRequest {
 
     public void setData(Map<String, String> data) {
         this.data = data;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
     }
 }
